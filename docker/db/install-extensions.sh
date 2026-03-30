48ce9874cc561e9502e02ce9cc64122e5806ee33
Zoya Fathima <zoyanoor2003@gmail.com>
2026-03-30 06:41:09 +0400

    fix: restore install-extensions.sh to original

--- docker/db/install-extensions.sh
+++ docker/db/install-extensions.sh
@@ -1,8 +1,7 @@
-#!/us/bin/env bash
-
+#!/usr/bin/env bash
 echo "Enabling extensions on database $POSTGRES_DB"
 psql -U $POSTGRES_USER --dbname="$POSTGRES_DB" <<-'EOSQL'
-  CREATE EXTENSION IF NOT EXISTS pg_tgm;
+  CREATE EXTENSION IF NOT EXISTS pg_trgm;
   CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 EOSQL
 echo "Finished with exit code $?"
